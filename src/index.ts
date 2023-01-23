7c7bd4c04deb35f3ba23e8776ef966f3f51afca8
Sedric <sedricgreen6@gmail.com>
2023-01-23 19:40:29 +0200

    feat: create webpack build

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
-import "../src/sass/index.scss";
-import App from "./view/app/app";
-
-const app = new App();
-app.run();
